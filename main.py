ad0af935d257474f9d310c1c257296aaf81ae43b
AlexAndHisScripts <alexcj2802@gmail.com>
2022-12-12 11:08:48 +0000

    fixes

--- main.py
+++ main.py
@@ -83,6 +83,9 @@ async def on_message(message):
                 print(quote)
                 txt, author = quote
 
+                if not quote or not author:
+                    continue
+
                 if "<@" in author:
                     author = author.replace("<@", "")
                     author = author.replace(">", "")
@@ -102,7 +105,8 @@ async def on_message(message):
                 viewID_to_messageID[viewid] = newmessage.id
                 viewID_to_content[viewid] = txt
                 viewID_to_author[viewid] = author
-                viewID_to_submitted_by[viewid] = message.author.nick if message.author.nick is not None else message.author.name
+                viewID_to_submitted_by[
+                    viewid] = message.author.nick if message.author.nick is not None else message.author.name
 
     await bot.process_commands(message)
 
@@ -113,6 +117,7 @@ viewID_to_author = {}
 viewID_to_submitted_by = {}
 have_approved = []
 
+
 class ApproveView(discord.ui.View):
     def __init__(self, viewid):
         self.viewid = viewid
@@ -121,14 +126,17 @@ class ApproveView(discord.ui.View):
     @discord.ui.button(label="Approve", style=discord.ButtonStyle.green)
     async def approve(self, button: discord.ui.Button, interaction: discord.Interaction):
         if self.viewid in have_approved:
-            await interaction.response.send_message("Race condition detected - someone else has approved it!", ephemeral=True)
+            await interaction.response.send_message("Race condition detected - someone else has approved it!",
+                                                    ephemeral=True)
             return
         have_approved.append(self.viewid)
         await interaction.response.send_message("Approved!", ephemeral=True)
         content = viewID_to_content[self.viewid]
         author = viewID_to_author[self.viewid]
         quotesChannel = bot.get_channel(quotechannel)
-        embed = discord.Embed(title=f"Quote from {viewID_to_submitted_by[self.viewid]} about {viewID_to_author[self.viewid]}", description='"' + content + '" - ' + author)
+        embed = discord.Embed(
+            title=f"Quote from {viewID_to_submitted_by[self.viewid]} about {viewID_to_author[self.viewid]}",
+            description='"' + content + '" - ' + author)
         await quotesChannel.send(embed=embed)
         await bot.get_message(viewID_to_messageID[self.viewid]).delete()
 
